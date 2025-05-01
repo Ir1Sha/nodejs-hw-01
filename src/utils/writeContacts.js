76e4cf98c025b90df29db35550185b71fbafb158
ginny <ir.gonccharova.up@gmail.com>
2025-05-01 16:45:32 +0300

    final commit before push

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,7 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const contactsData = JSON.stringify(updatedContacts, undefined, 2);
+  await fs.writeFile(PATH_DB, contactsData, 'utf-8');
+};
